96f81d9abdc8debe87a5227a36aacc69ea367083
James Hooper <60763364+jamesH-48@users.noreply.github.com>
2021-01-14 22:08:25 -0600

    Update study_helper.pl

--- study_helper.pl
+++ study_helper.pl
@@ -1,48 +1,74 @@
 /* Instantiations */
+% student
+student(stu).
+
 % classes
 class(intro_to_logic).
 class(os_prog).
 class(javaII).
 class(texas_gov).
 
+% taking
+% student is taking class with grade g
+taking(stu,intro_to_logic,95).
+taking(stu,os_prog,85).
+taking(stu,javaII,90).
+taking(stu,texas_gov,75).
+
 % class gives coursework due in x days
 coursework(intro_to_logic,logic_quiz,5).
 quiz(logic_quiz).
 coursework(os_prog,os_final,15).
 final(os_final).
+coursework(javaII,objectHW,8).
+homework(objectHW).
+coursework(texas_gov,unit1,6).
+quiz(unit1).
+coursework(texas_gov,unit2,12).
+quiz(unit2).
 
 /* Time Urgency */
 % past 3 weeks we assume an urgency of 2
 time_urgency(X,2) :- coursework(_,X,Z), Z >= 21.
 time_urgency(X,4) :- coursework(_,X,Z), Z >= 14, Z < 21.
 time_urgency(X,6) :- coursework(_,X,Z), Z >= 7, Z < 14.
-time_urgency(X,12) :- coursework(_,X,Z), Z >= 4, Z < 7.
-time_urgency(X,24) :- coursework(_,X_,Z), Z >= 0, Z < 4.
+time_urgency(X,8) :- coursework(_,X,Z), Z >= 4, Z < 7.
+time_urgency(X,10) :- coursework(_,X,Z), Z >= 0, Z < 4.
+
+
+/* Grade Urgency */
+grade_urgency(X,0) :- taking(_,X,Z), Z >= 93.
+grade_urgency(X,3) :- taking(_,X,Z), Z >= 90, Z < 93.
+grade_urgency(X,6) :- taking(_,X,Z), Z >= 85, Z < 90.
+grade_urgency(X,9) :- taking(_,X,Z), Z >= 80, Z < 85.
+grade_urgency(X,18) :- taking(_,X,Z), Z >= 75, Z < 80.
+grade_urgency(X,27) :- taking(_,X,Z), Z >= 70, Z < 75.
+grade_urgency(X,36) :- taking(_,X,Z), Z < 70.
 
 /* Assignments */
 assignment(X) :- homework(X).
 assignment(X) :- quiz(X).
 assignment(X) :- project(X).
 
-/* Assignment Urgency */
-a_urgency(X,5) :- homework(X).
+/* Assignment Urgency */ 
+a_urgency(X,5) :- homework(X).  
 a_urgency(X,10) :- quiz(X).
 a_urgency(X,15) :- project(X).
-
+         
 /* Exams */
 exam(X) :- midterm(X).
 exam(X) :- final(X).
-
+      
 /* Exam Urgency */
 % this means to work on / study for
 % actions can be different such as with quizzes (studying for or completing)
 % the functional difference doesn't really matter as long as assume this is study overall
 e_urgency(X,20) :- midterm(X).
-e_urgency(X,30) :- final(X).
-
-/* Urgency Total */
-urgency(X,Y) :- time_urgency(X,B), a_urgency(X,D), assignment(X), Y is B+D.
-urgency(X,Y) :- time_urgency(X,B), e_urgency(X,D), exam(X), Y is B+D.
+e_urgency(X,30) :- final(X). 
+    
+/* Urgency Total */ 
+urgency(X,Y) :- time_urgency(X,B), a_urgency(X,C), taking(_,Z,_), grade_urgency(Z,D), coursework(Z,X,_), assignment(X), Y is B+C+D.
+urgency(X,Y) :- time_urgency(X,B), e_urgency(X,C), taking(_,Z,_), grade_urgency(Z,D), coursework(Z,X,_), exam(X), Y is B+C+D.
 
 /* Urgency List */
 urgencyList(L) :- findall([X,Y], urgency(X,Y), L).

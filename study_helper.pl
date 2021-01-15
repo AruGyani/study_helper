5c7efe57eb155cbddd19f852ebc30e92e872fb09
James Hooper <60763364+jamesH-48@users.noreply.github.com>
2021-01-14 23:38:46 -0600

    Update study_helper.pl

--- study_helper.pl
+++ study_helper.pl
@@ -1,31 +1,5 @@
 /* Instantiations */
-% student
-student(stu).
-
-% classes
-class(intro_to_logic).
-class(os_prog).
-class(javaII).
-class(texas_gov).
-
-% taking
-% student is taking class with grade g
-taking(stu,intro_to_logic,95).
-taking(stu,os_prog,85).
-taking(stu,javaII,90).
-taking(stu,texas_gov,75).
-
-% class gives coursework due in x days
-coursework(intro_to_logic,logic_quiz,5).
-quiz(logic_quiz).
-coursework(os_prog,os_final,15).
-final(os_final).
-coursework(javaII,objectHW,8).
-homework(objectHW).
-coursework(texas_gov,unit1,6).
-quiz(unit1).
-coursework(texas_gov,unit2,12).
-quiz(unit2).
+#include 'header.pl'.
 
 /* Time Urgency */
 % past 3 weeks we assume an urgency of 2
@@ -34,7 +8,6 @@ time_urgency(X,4) :- coursework(_,X,Z), Z >= 14, Z < 21.
 time_urgency(X,6) :- coursework(_,X,Z), Z >= 7, Z < 14.
 time_urgency(X,8) :- coursework(_,X,Z), Z >= 4, Z < 7.
 time_urgency(X,10) :- coursework(_,X,Z), Z >= 0, Z < 4.
-
 
 /* Grade Urgency */
 grade_urgency(X,0) :- taking(_,X,Z), Z >= 93.
@@ -73,7 +46,7 @@ urgency(X,Y,Z,A) :- time_urgency(X,B), e_urgency(X,C), taking(_,Z,_), grade_urge
 /* Urgency List */
 % X = Assignment Name
 % Y = Urgency Rating
-% Z = Class NAme
+% Z = Class Name
 % A = Due Date
 
 urgencyList(L) :- findall([X,Y,Z,A], urgency(X,Y,Z,A), L).
